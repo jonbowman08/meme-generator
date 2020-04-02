c015c0d6309c8fb8b45779a890c329d6254b2282
jonbowman08 <jonbowman999@gmail.com>
2020-04-02 20:28:24 +0100

    imnplementing

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import "./index.css";
 
-const App = () => {
-  return <div>App</div>;
-};
+class App extends React.Component {
+  componentDidMount() {
+    const context = this.canvasA.getContext("2d");
+
+    const image = new Image();
+    image.src = "mocking-spongebob-1556133078.jpg";
+    image.onload = () => {
+      context.drawImage(image, 0, 0, this.canvasA.width, this.canvasA.height);
+      context.font = "30px Impact";
+      context.strokeText("Hello World", 75, 30);
+    };
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <input className="meme-input" />
+        <canvas
+          className="spongebob"
+          ref={canvasA => (this.canvasA = canvasA)}
+        ></canvas>
+      </div>
+    );
+  }
+}
 
 export default App;
